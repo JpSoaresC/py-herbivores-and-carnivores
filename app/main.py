9b165d08e4a5925a63d584a0f5fb1ce077694b3c
JpSoaresC <jsoaresngc@gmail.com>
2026-09-15 23:38:57 -0300

    Alterações nas linhas 10, 11, 15, 20

--- app/main.py
+++ app/main.py
@@ -7,17 +7,18 @@ class Animal:
         self.hidden = hidden
         Animal.alive.append(self)
 
-    def __repr__(self):
-        return f'{{Name: {self.name}, Health: {self.health}, Hidden: {self.hidden}}}'
-
+    def __repr__(self) -> str:
+        return (f"{{Name: {self.name}, Health: {self.health}, "
+                f"Hidden: {self.hidden}}}"
+        )
 
 class Herbivore(Animal):
-    def hide(self):
+    def hide(self) -> None:
         self.hidden = not self.hidden
 
 
 class Carnivore(Animal):
-    def bite(self, target) -> None:
+    def bite(self, target: "Animal") -> None:
         if isinstance(target, Carnivore):
             return
         if target.hidden:
